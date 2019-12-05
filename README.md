6857a64feacfca5340fae095d647b56887f209cd
sminamot <shosfs@gmail.com>
2019-12-05 19:38:04 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+### 実行方法
+```
+$ SHEET_ID=<spreadsheetID> go run main.go
+```
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
+)
+
+func main() {
+	secret, err := ioutil.ReadFile("secret.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf, err := google.JWTConfigFromJSON(secret, sheets.SpreadsheetsScope)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := conf.Client(context.Background())
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	spreadsheetID := os.Getenv("SHEET_ID")
+
+	// 参照
+	readRange := "Sheet1!A:B"
+	resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(resp.Values) == 0 {
+		fmt.Println("No data found.")
+		return
+	}
+	for _, row := range resp.Values {
+		fmt.Printf("%s, %s\n", row[0], row[1])
+	}
+
+	// 更新
+	var vr sheets.ValueRange
+	now := time.Now().Format("2006/01/02 15:04:05")
+	for i := 0; i < len(resp.Values)-1; i++ {
+		vr.Values = append(vr.Values, []interface{}{now})
+	}
+	updateRange := "Sheet1!B2:B"
+	if _, err = srv.Spreadsheets.Values.Update(spreadsheetID, updateRange, &vr).ValueInputOption("RAW").Do(); err != nil {
+		log.Fatal(err)
+	}
+}
